4306bd69abed10639b9526e19c15d8c24cf77d16
liusj_account@163.com <9070160+gopherliu@users.noreply.github.com>
2023-03-03 18:02:25 +0800

    f

--- server.go
+++ server.go
@@ -1,6 +1,18 @@
 package nostr_relay
 
+import (
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+)
+
 type Server struct {
 	addr  string
 	relay Relay
+
+	router     *mux.Router
+	httpServer *http.Server
+
+	clientsMu sync.Mutex
 }
